548244400a28bb1e2d33e5d1a108309d99c50c1f
erathe <eirik.rathe@gmail.com>
2018-12-01 21:03:40 +0100

    added fetching of input

--- fileGenerator.js
+++ fileGenerator.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const fetch = require('node-fetch')
+const session = require('./session.js');
 const day = process.argv[2];
 
 const template = `'use strict';
@@ -15,24 +17,41 @@ console.timeEnd('run')`;
 const filesToCreate = [
   {
     filename: `solution-${day}.js`,
-    content: template
+    content: template,
+    getData: false
   },
   {
     filename: `input${day}`,
-    content: ''
+    content: '',
+    getData: true
   }
 ];
 
-const createFile = (filename, content) => {
+const getInput = async session => {
+  const opts = {
+    headers: {
+      cookie: `session=${session}`
+    }
+  };
+  const result = await fetch(
+    `https://adventofcode.com/2018/day/${day}/input`,
+    opts
+  );
+  return await result.text()
+};
+
+const createFile = async (filename, content, getData) => {
+  if (getData) {
+    content = await getInput(session)
+  }
   fs.writeFile(filename, content, err => {
     if (err) {
       console.log(err);
     }
-
     console.log(`file ${filename} was created`);
   });
 };
 
 for (let file of filesToCreate) {
-  createFile(file.filename, file.content);
+  createFile(file.filename, file.content, file.getData);
 }
